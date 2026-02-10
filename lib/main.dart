537dd7ab94530cef23700a9b8ae3a2f6adb42412
--global <yasmin.emad600@gmail.com>
2026-02-10 02:01:47 +0200

    feat: Initialize Firebase and set up routing for user and admin screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:app1/features/auth/data/repo/firebase_repo.dart';
+import 'package:app1/features/auth/presentation/view/pages/admin_home_screen.dart';
+import 'package:app1/features/auth/presentation/view/pages/admin_login_screen.dart';
+import 'package:app1/features/auth/presentation/view/pages/admin_signup_screen.dart';
+import 'package:app1/features/auth/presentation/view/pages/user_home_screen.dart';
+import 'package:app1/features/auth/presentation/view/pages/user_login_screen.dart';
+import 'package:app1/features/auth/presentation/view/pages/user_profile_screen.dart';
+import 'package:app1/features/auth/presentation/view/pages/user_signup_screen.dart';
+import 'package:app1/features/auth/presentation/view_model/auth.cubit.dart';
+import 'package:app1/features/splash_screen.dart';
+import 'package:app1/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'features/admin/trips/presentation/view/pages/add_trip_page.dart';
-
-void main() {
+import 'package:flutter_bloc/flutter_bloc.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,15 +25,75 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Trip Admin',
-      theme: ThemeData(
-        fontFamily: 'Arial',
+    return RepositoryProvider(
+      create: (context) => AuthRepository(),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AuthCubit(
+              RepositoryProvider.of<AuthRepository>(context),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'TravelX',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            useMaterial3: true,
+            fontFamily: 'Roboto',
+          ),
+          onGenerateRoute: (settings) {
+            switch (settings.name) {
+              case '/':
+                return MaterialPageRoute(
+                  builder: (_) => const SplashScreen(),
+                );
+              case '/user_login':
+                return MaterialPageRoute(
+                  builder: (_) =>  UserLoginScreen(),
+                );
+              case '/user_signup':
+                return MaterialPageRoute(
+                  builder: (_) => const UserSignupScreen(),
+                );
+              case '/user_home':
+                return MaterialPageRoute(
+                  builder: (_) => const UserHomeScreen(),
+                );
+              case '/user_profile':
+                return MaterialPageRoute(
+                  builder: (_) => const UserProfileScreen(),
+                );
+              // case '/admin_profile':
+              // return MaterialPageRoute(
+              //   builder: (_) => const UserProfileScreen(),
+              // );
+              case '/admin_home':
+                return MaterialPageRoute(
+                  builder: (_) => const AdminHomeScreen(),
+                );
+              case '/admin_login':
+                return MaterialPageRoute(
+                  builder: (_) => const AdminLoginScreen(),
+                );
+              case '/admin_signup':
+                return MaterialPageRoute(
+                  builder: (_) => const AdminSignupScreen(),
+                );
+              // case '/add_trip':
+              //   return MaterialPageRoute(
+              //     builder: (_) => const AddTripScreen(),
+              //   );
+              default:
+                return MaterialPageRoute(
+                  builder: (_) => const SplashScreen(), //homeScreen(),
+                );
+            }
+          },
+        ),
       ),
-      home: const AddTripScreen(),
     );
   }
 }
-
 
