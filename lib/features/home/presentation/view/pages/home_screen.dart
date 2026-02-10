8991288180e8d84d54e064d07c25c3345ae0d4dc
Nourhan Khaled <elgendynour529@gmail.com>
2026-02-10 02:36:46 +0200

    user home screen & main

---
+++ lib/features/home/presentation/view/pages/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/text_style.dart';
+import '../../../data/home_card_model.dart';
+import '../widgets/app_bar_widget.dart';
+import '../widgets/home_trip_card.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = "homeScreen";
+  HomeScreen({super.key});
+
+  final List<HomeCardModel> tripData =[
+    HomeCardModel("assets/images/alex.jpg", "Alexandria", " From Alex", " Jan, 13, 2026", " 6:00 AM", "200 EGP")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: AppColors.surface,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.2 * height),
+        child: AppBarWidget(),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text("Available Trips", style: AppTextStyles.h2Heading),
+            ),
+            TripCardHome(tripData[0]),
+            TripCardHome(tripData[0]),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'features/admin/trips/presentation/view/pages/add_trip_page.dart';
+import 'features/user/data/home_card_model.dart';
+import 'features/user/presentation/view/pages/book_page.dart';
+import 'features/user/presentation/view/pages/booking_confirmed.dart';
+import 'features/user/presentation/view/pages/home_page.dart';
+import 'features/user/presentation/view/pages/home_screen.dart';
+import 'features/user/presentation/view/pages/trip_details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +17,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Trip Admin',
-      theme: ThemeData(
-        fontFamily: 'Arial',
-      ),
-      home: const AddTripScreen(),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+        HomeScreen.routeName: (context) => HomeScreen(),
+        TripDetails.routeName: (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as HomeCardModel;
+          return TripDetails(homeCard: args);
+        },
+        BookPage.routeName:(context) => BookPage(),
+        BookingConfirmed.routeName:(context) => BookingConfirmed()
+      },
     );
   }
 }
-
 
